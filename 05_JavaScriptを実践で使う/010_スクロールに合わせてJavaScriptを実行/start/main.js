a95aaa0abd94dafffcd8c95e5f2ffcaa4965537b
keisuke-sakuma <sakumalittle@yahoo.co.jp>
2020-07-29 12:16:48 +0900

    修正

--- 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
+++ 05_JavaScriptを実践で使う/010_スクロールに合わせてJavaScriptを実行/start/main.js
@@ -1,30 +1,19 @@
-//1.メソッドの関数式を作成する
-//2.監視したいオブジェクトを変数として用いる
-//3.作成した変数に監視対象のオブジェクトを指定する
-
+//④コールバック関数を作成する
 const cb = function(entries, observer){
-    entries.forEach(entry){
-        if(entry.isInterSecting){
-        //true
-        console.log('in view');
-        }
-        else{
-        // false
-        console.log('out view');
-        }
+   entries.forEach(entry => {
+       if(entry.isIntersecting){
+           entry.target.classlist.add('inview');
+       }else{
+           entry.target.classlist.remove('inview');
+       }
+});
 
-    }
-}
+//③ DOMで監視対象のインスタンスを生成する
 
-//1.メソッドの関数式を作成する
-const io = new IntersectionObserver(cb);
-
-//2.監視したいオブジェクトを変数として用いる
 const child = window.document.querySelector('.child');
 
+//①IntersectionObserverクラスのオブジェクトをインスタンス化する
+const io = new IntersectionObserver(cb);
 
-//3.作成した変数に監視対象のオブジェクトを指定する
+//② インスタンスオブジェクトにobserveメソッドで監視対象のクラスを指定する。
 io.observe(child);
-
-// インターセクションオブザーバーメソッドには２つの関数を指定することができる。
-//→entries observer
